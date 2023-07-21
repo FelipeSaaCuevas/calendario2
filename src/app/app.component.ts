7694d5d14fd64d0c51c2de6e7868b46221e69c3d
FelipeSaaCuevas <133611945+FelipeSaaCuevas@users.noreply.github.com>
2023-07-21 14:42:37 -0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  titulo= 'calendario2';
+  semana= ["lunes","","miercoles","jueves","viernes","sabado","domingo"]
+  Semana1= ["lunes","martes","mierciles","miercoles","jueves","viernes","sabado","domingo"];
+ 
+}
